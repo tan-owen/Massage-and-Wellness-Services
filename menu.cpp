ec67f10f37eafab42582524344d56cb374ae5af4
dashquack <tanowen05@gmail.com>
2025-07-23 23:51:24 +0800

    update

---
+++ menu.cpp
@@ -0,0 +1,6 @@
+#include "function.hpp"
+using namespace std;
+
+string organizationLogoScreen() {
+    return "test";
+}
--- menu.hpp
+++ menu.hpp
@@ -5,42 +5,29 @@
 using namespace std;
 
 
-void organizationLogoScreen() {
-    cout << "test";
-    //std::cout << "\n\n";
-    //std::cout << "  --------------------------------------------------\n";
-    //std::cout << " |                                                  |\n";
-    //std::cout << " |         ** Massage and Wellness ** |\n";
-    //std::cout << " |                                                  |\n";
-    //std::cout << " |           * Your Journey to Serenity * |\n";
-    //std::cout << " |                                                  |\n";
-    //std::cout << " |         Rest | Rejuvenate | Renew              |\n";
-    //std::cout << " |                                                  |\n";
-    //std::cout << "  --------------------------------------------------\n";
-    //std::cout << "\n\n";
-}
-//void loginScreen() {}
-//void registrationScreen() {}
-//
-////user screens
-//void customerOptionScreen() {}
-//void appointmentSelectScreen() {}
-//void serviceSelectScreen() {}
-//void expertSelectScreen() {}
-//void dateSelectScreen() {}
-//void timeSlotSelectScreen() {}
-//void paymentMethodSelectScreen() {}
-//void paymentSuccessScreen() {}
-//void viewBookingScreen() {}
-//
-////admin screens
-//void adminOptionScreen() {}
-//void viewOverallScheduleScreen() {}
-//void viewCustomerListScreen() {}
-//void generateSalesReportScreen() {}
-//void viewExpertBonusScreens_ADMIN() {}
-//
-////expert screens
-//void viewScheduleScreen() {}
-//void viewAssignedCustomerScreen() {}
-//void viewEarningBonusScreen_EXPERT() {}
+string organizationLogoScreen()  ;
+void loginScreen();
+void registrationScreen();
+
+//user screens
+void customerOptionScreen();
+void appointmentSelectScreen();
+void serviceSelectScreen();
+void expertSelectScreen();
+void dateSelectScreen();
+void timeSlotSelectScreen();
+void paymentMethodSelectScreen();
+void paymentSuccessScreen();
+void viewBookingScreen();
+
+//admin screens
+void adminOptionScreen();
+void viewOverallScheduleScreen();
+void viewCustomerListScreen();
+void generateSalesReportScreen();
+void viewExpertBonusScreens_ADMIN();
+
+//expert screens
+void viewScheduleScreen();
+void viewAssignedCustomerScreen();
+void viewEarningBonusScreen_EXPERT();
