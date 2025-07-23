3b06236e9e1da3b38e453fa0fdd44227773af84d
TARUMT <TARUMT@B195>
2025-07-23 12:43:22 +0800

    rename function.hpp to menu.hpp, added function.hpp

---
+++ menu.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void loginScreen() {}
+
+//user screens
+void customerOptionScreen() {}
+void appointmentSelectScreen() {}
+void serviceSelectScreen() {}
+void expertSelectScreen() {}
+void dateSelectScreen() {}
+void timeSlotSelectScreen() {}
+void paymentMethodSelectScreen() {}
+void paymentSuccessScreen() {}
+void viewBookingScreen() {}
+
+//admin screens
+void adminOptionScreen() {}
+void viewOverallScheduleScreen() {}
+void viewCustomerListScreen() {}
+void generateSalesReportScreen() {}
+void viewExpertBonusScreens_ADMIN() {}
+
+//expert screens
+void viewScheduleScreen() {}
+void viewAssignedCustomerScreen() {}
+void viewEarningBonusScreen_EXPERT() {}
