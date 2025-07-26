53c74891fe849f2489c973c535844d80cb0f4e0c
lebin_26 <lebin2626@gmail.com>
2025-07-26 13:50:24 +0800

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,42 @@
 #include "function.hpp"
 using namespace std;
 
-string organizationLogoScreen() {
-    return "test";
-}
+
+void dateSelectScreen(){
+// 
+};
+void timeSlotSelectScreen(){
+    cout<<"Select a time slot for your appointment:"<< endl;
+
+    // cout<<"1. 0900 - 1000"<<endl;
+    //cout<<"2. 1000 - 1100"<<endl;
+
+};
+void paymentMethodSelectScreen(){
+    cout << "Select a payment method:" << endl;
+    // cout << "1. Cash" << endl;
+    // cout << "2. Debit & Credit Card" << endl;
+    // cout << "3. Touch N Go" << endl;
+    // cout << "4. Bank Transfer" << endl;
+};
+
+void paymentSuccessScreen(){
+    cout << "Payment successful!" << endl;
+    cout << "Thank you for your payment." << endl;
+};
+
+void viewBookingScreen(){
+    cout << "Here are your booking details:" << endl;
+};
+
+void adminOptionScreen(){
+    // cout << "Admin Options:" << endl;
+    // cout << "1. View All Appointments" << endl;
+    // cout << "2. Manage Accounts" << endl;
+    // cout << "3. View Overall Schedule" << endl;
+    // cout << "4. Exit" << endl;
+};
+
+void viewOverallScheduleScreen(){
+    cout << "Overall Schedule:" << endl;
+};
