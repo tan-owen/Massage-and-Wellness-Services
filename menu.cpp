66ad67bd01d79bbbea44306914a40e1e651096b9
Loongsx <166084405+Loongsx@users.noreply.github.com>
2025-07-26 14:29:24 +0800

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -40,3 +40,46 @@ void adminOptionScreen(){
 void viewOverallScheduleScreen(){
     cout << "Overall Schedule:" << endl;
 };
+
+// LSX
+
+//admin screen
+void viewCustomerListScreen();
+    //cout << "List of Customers\n"; 
+    //cout << "--------------------------------------------------------\n";
+    //cout << left << setw(5) << "No. |" << setw(30) << "Customer Name" << setw(8) << "Date" << setw(6) << "Time" << setw(20) << "Payment Details" << setw(30) << "Expert Name" << setw(20) << "Service Type" << "\n";
+    //cout << "--------------------------------------------------------\n";
+    //cout << left << setw(5) << "1. " << setw(30) << "Name Name Name" << setw(8) << "Dec 12" << setw(6) << "1400" << setw(20) << "Credit Card" << setw(30) << "Expert Name Name" << setw(20) << "Massage" << "\n";
+    //cout << left << setw(5) << "2. " << setw(30) << "Name Name Name" << setw(8) << "Dec 12" << setw(6) << "1400" << setw(20) << "Credit Card" << setw(30) << "Expert Name Name" << setw(20) << "Massage" << "\n";
+
+void generateSalesReportScreen();
+    //cout << "Sales Report\n";
+    //cout << "--------------------------------------------------------\n";
+    //cout << "1. Date Range\n";
+    //cout << "2. Service Category\n";
+    //cout << "3. Expert\n\n";
+    //cout << "Select from the option above (1 - 3): \n\n";
+    
+    //cout << "Enter date range: \n\n";
+    //cout << "Total sales for date range: $20000 \n\n";
+    
+    //cout << "Enter service category: \n\n";
+    //cout << "Total sales for service category: $20101 \n\n";
+    
+    //cout << "Enter expert name: \n\n";
+    //cout << "Total sales for expert name: $10000 \n\n";
+
+
+void viewExpertBonusScreens_ADMIN();
+
+
+
+//expert screens
+void viewScheduleScreen();
+
+
+void viewAssignedCustomerScreen();
+
+
+void viewEarningBonusScreen_EXPERT();
+
