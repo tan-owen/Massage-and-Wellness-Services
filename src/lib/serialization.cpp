8513d9d0eb902fd69f0f70627a1f7dada64e9526
Tiffceet <zkang.loo@gmail.com>
2025-08-12 16:06:47 +0800

    feat(serialization): support more type

--- src/lib/serialization.cpp
+++ src/lib/serialization.cpp
@@ -1,7 +1,7 @@
 #include "serialization.hpp"
 #include <string>
 
-std::string deserializeString(std::string inputDataString, std::string key)
+std::string getStringValueByKey(std::string inputDataString, std::string key)
 {
     std::size_t keyIndex = inputDataString.find(key + "=");
     std::size_t keyLength = std::string(key + "=").length();
@@ -10,8 +10,62 @@ std::string deserializeString(std::string inputDataString, std::string key)
     return inputDataString.substr(dataStartIndex, delimiterIndex - dataStartIndex);
 }
 
+int getIntValueByKey(std::string inputDataString, std::string key)
+{
+    std::string stringValue = getStringValueByKey(inputDataString, key);
+    return std::stoi(stringValue);
+}
+
+double getDoubleValueByKey(std::string inputDataString, std::string key)
+{
+    std::string stringValue = getStringValueByKey(inputDataString, key);
+    return std::stod(stringValue);
+}
+
+float getFloatValueByKey(std::string inputDataString, std::string key)
+{
+    std::string stringValue = getStringValueByKey(inputDataString, key);
+    return std::stof(stringValue);
+}
+
+char getCharValueByKey(std::string inputDataString, std::string key)
+{
+    std::string stringValue = getStringValueByKey(inputDataString, key);
+    return stringValue.at(0);
+}
+
+bool getBoolValueByKey(std::string inputDataString, std::string key)
+{
+    int intValue = getIntValueByKey(inputDataString, key);
+    return intValue == 1;
+}
+
 std::string serializeString(std::string data, std::string key)
 {
-    std::string outputString = key + "=" + data + ";";
-    return outputString;
+    return key + "=" + data + ";";
+}
+
+std::string serializeInt(int data, std::string key)
+{
+    return key + "=" + std::to_string(data);
+}
+
+std::string serializeDouble(double data, std::string key)
+{
+    return key + "=" + std::to_string(data);
+}
+
+std::string serializeFloat(float data, std::string key)
+{
+    return key + "=" + std::to_string(data);
+}
+
+std::string serializeChar(char data, std::string key)
+{
+    return key + "=" + std::to_string(data);
+}
+
+std::string serializeBool(bool data, std::string key)
+{
+    return key + "=" + std::to_string(data);
 }
--- src/lib/serialization.hpp
+++ src/lib/serialization.hpp
@@ -1,4 +1,16 @@
 #pragma once
 #include <string>
-std::string deserializeString(std::string inputDataString, std::string key);
+
+std::string getStringValueByKey(std::string inputDataString, std::string key);
+int getIntValueByKey(std::string inputDataString, std::string key);
+double getDoubleValueByKey(std::string inputDataString, std::string key);
+float getFloatValueByKey(std::string inputDataString, std::string key);
+char getCharValueByKey(std::string inputDataString, std::string key);
+bool getBoolValueByKey(std::string inputDataString, std::string key);
+
 std::string serializeString(std::string data, std::string key);
+std::string serializeInt(int data, std::string key);
+std::string serializeDouble(double data, std::string key);
+std::string serializeFloat(float data, std::string key);
+std::string serializeChar(char data, std::string key);
+std::string serializeBool(bool data, std::string key);
